4f681013cc65b5b8b3e7d766a3a4515868c2adbf
Marioisdabest <mtutuncumacias1@gmail.com>
2019-12-03 15:28:38 -0500

    Added template and file names

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int main(){
+
+  return 0;
+}
