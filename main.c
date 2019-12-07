df8d915cd8e8cca1a05d82af55e204de731a267b
MarioTuMa <mtutuncumacias1@gmail.com>
2019-12-07 11:28:39 -0500

    added cd

--- main.c
+++ main.c
@@ -8,22 +8,28 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <errno.h>
-
 #include "shellfuncs.h"
 
 int main(int argc, char **argv){
   while(1){
-    printf("Enter Your Command: ");
+    char cwd[4096];
+    printf("%s>", getcwd(cwd, 4096));
     char directString[4096];
     int limit=4096;
     fgets(directString, limit, stdin);
     directString[strlen(directString)-1]='\0';
-    //printf("%s\n",directString);
     char** callParams=parse_args(directString);
-    //printf("made it here \n");
-    //printf("%s\n", (callParams[0]));
+    char cd[2] = "cd";
+    //printf("%s\n", callParams[0]);
+    //printf("%d\n", strcmp(callParams[0],cd));
+    if(!strcmp(callParams[0],cd)){
+      //printf("Hi, we don't know how to cd yet\n");
+      chdir(callParams[1]);
+    }
+    else{
+      launch_process(callParams);
+    }
 
-    launch_process(callParams);
   }
   return 0;
 }
